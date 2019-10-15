2187fcd29d8c108ecddbee9ef5a470059d53189c
Hauke Lüdemann <h.luedemann97@gmail.com>
2019-10-15 21:54:48 +0200

    Added the code from the part 1 of exercise 1.

--- exercise1/part1.cpp
+++ exercise1/part1.cpp
@@ -2,7 +2,36 @@
 
 int main()
 {
-    std::cout << "Part 1" << std::endl;
+    /// Number 1.1
+    {
+        int i = 7;
+        float y = 2 * (i / 2);      // -> Integer devicion cuts of the floating part
+        float z = 2 * (i / 2.);
 
+        printf("Part 1.1:\n");
+        printf("%e %e \n", y, z);
+    }
+
+    /// Number 1.2
+    {
+        double a = 1.0e17;
+        double b = -1.0e17;
+        double c = 1.0;
+        double x = (a + b) + c;
+        double y = a + (b + c);
+
+        printf("Part 1.2:\n");
+        printf("%e %e \n", x, y);
+    }
+
+    /// Number 1.3
+    {
+        float x = 1e20;
+        float y;
+        y = x * x;      // -> Overflow, hence devision by zero
+
+        printf("Part 1.3:\n");
+        printf("%e %e\n", x, x / y);
+    }
     return 0;
 }
